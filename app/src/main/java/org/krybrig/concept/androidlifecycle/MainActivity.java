79e006dcb9270ddf63e6c2e129ae75d9fa7f387e
Erdian Setyadi <kassle@krybrig.org>
2019-05-03 14:47:25 +0700

    implement ProgressDialog

--- app/src/main/java/org/krybrig/concept/androidlifecycle/MainActivity.java
+++ app/src/main/java/org/krybrig/concept/androidlifecycle/MainActivity.java
@@ -1,10 +1,12 @@
 package org.krybrig.concept.androidlifecycle;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -14,16 +16,23 @@ import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String STATE_KEY_PROGRESSBAR = "org.krybrig.state.progressbar";
+    private static final String STATE_KEY_PROGRESS = "org.krybrig.state.progress";
     private static final String STATE_KEY_BUTTON = "org.krybrig.state.button";
     private static final String STATE_KEY_RESUME_LIVE_DATA = "org.krybrig.state.livedata";
+    private static final String STATE_KEY_PROGRESSBAR_AS_DIALOG = "org.krybrig.state.progressAsDialog";
+    private static final String STATE_KEY_CHECKBOX_DIALOG = "org.krybrig.state.checkbox";
     private static final int MAX_PROGRESSBAR = 10;
 
     private boolean resume;
+
     private DataProducer producer;
     private Button button;
+    private CheckBox cbProgressAsDialog;
+
     private ProgressBar pgbar;
+    private ProgressDialog pgdialog;
     private Disposable disposable;
+    private int progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,17 +42,36 @@ public class MainActivity extends AppCompatActivity {
         producer = new DataProducer();
 
         button = findViewById(R.id.btCount);
+        cbProgressAsDialog = findViewById(R.id.cbProgressDialog);
+
         pgbar = findViewById(R.id.pgbar);
         pgbar.setMax(MAX_PROGRESSBAR);
 
-        if (savedInstanceState != null) {
-            int progress = savedInstanceState.getInt(STATE_KEY_PROGRESSBAR, 0);
-            pgbar.setProgress(progress);
-            button.setEnabled(savedInstanceState.getBoolean(STATE_KEY_BUTTON, true));
-            resume = savedInstanceState.getBoolean(STATE_KEY_RESUME_LIVE_DATA, false);
+        pgdialog = new ProgressDialog(this);
+        pgdialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        pgdialog.setCancelable(false);
+        pgdialog.setCanceledOnTouchOutside(false);
+        pgdialog.setMax(MAX_PROGRESSBAR);
 
-            Log.w("COUNTER", "Load progress: " + pgbar.getProgress() + "; button = " + button.isEnabled() + "; resume = " + resume);
+        if (savedInstanceState != null) {
+            restoreViewState(savedInstanceState);
         }
+    }
+
+    private void restoreViewState(Bundle savedInstanceState) {
+        boolean asDialog = savedInstanceState.getBoolean(STATE_KEY_PROGRESSBAR_AS_DIALOG, false);
+        cbProgressAsDialog.setChecked(asDialog);
+        cbProgressAsDialog.setEnabled(savedInstanceState.getBoolean(STATE_KEY_CHECKBOX_DIALOG, true));
+        button.setEnabled(savedInstanceState.getBoolean(STATE_KEY_BUTTON, true));
+        resume = savedInstanceState.getBoolean(STATE_KEY_RESUME_LIVE_DATA, false);
+
+        progress = savedInstanceState.getInt(STATE_KEY_PROGRESS, 0);
+        if (asDialog) {
+            pgdialog.setProgress(progress);
+        } else {
+            pgbar.setProgress(progress);
+        }
+        Log.w("COUNTER", "Load progress: " + pgbar.getProgress() + "; button = " + button.isEnabled() + "; resume = " + resume);
     }
 
     @Override
@@ -76,7 +104,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putBoolean(STATE_KEY_BUTTON, button.isEnabled());
-        outState.putInt(STATE_KEY_PROGRESSBAR, pgbar.getProgress());
+        outState.putInt(STATE_KEY_PROGRESS, pgbar.getProgress());
+        outState.putBoolean(STATE_KEY_PROGRESSBAR_AS_DIALOG, cbProgressAsDialog.isChecked());
+        outState.putBoolean(STATE_KEY_CHECKBOX_DIALOG, cbProgressAsDialog.isEnabled());
         outState.putBoolean(STATE_KEY_RESUME_LIVE_DATA, resume);
 
         super.onSaveInstanceState(outState);
@@ -85,7 +115,11 @@ public class MainActivity extends AppCompatActivity {
     private void startLiveData() {
         resume = true;
         button.setEnabled(false);
-        pgbar.setProgress(0);
+        cbProgressAsDialog.setEnabled(false);
+
+        progress = 0;
+        pgbar.setProgress(progress);
+        pgdialog.setProgress(progress);
 
         ValueHolder.getInstance().setObservableSource(producer.retrieve(MAX_PROGRESSBAR));
         resumeLiveData();
@@ -99,16 +133,43 @@ public class MainActivity extends AppCompatActivity {
                     public void run() throws Exception {
                         button.setEnabled(true);
                         resume = false;
+                        cbProgressAsDialog.setEnabled(true);
                         Toast.makeText(MainActivity.this, R.string.toast_task_completed, Toast.LENGTH_SHORT).show();
                         Log.w("COUNTER", "Completed");
                     }
                 })
+                .doFinally(new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        if (cbProgressAsDialog.isChecked()) {
+                            pgdialog.dismiss();
+                        }
+                    }
+                })
+                .doOnSubscribe(new Consumer<Disposable>() {
+                    @Override
+                    public void accept(Disposable disposable) throws Exception {
+                        if (cbProgressAsDialog.isChecked()) {
+                            pgdialog.show();
+                        }
+
+                        updateProgress(progress);
+                    }
+                })
                 .subscribe(new Consumer<Integer>() {
                     @Override
-                    public void accept(Integer integer) throws Exception {
-                        pgbar.setProgress(integer);
-                        Log.w("COUNTER", "Live data " + integer);
+                    public void accept(Integer progress) throws Exception {
+                        Log.w("COUNTER", "Live data " + progress);
+                        updateProgress(progress);
                     }
                 });
+    }
+
+    private void updateProgress(int progress) {
+        if (cbProgressAsDialog.isChecked()) {
+            pgdialog.setProgress(progress);
+        } else {
+            pgbar.setProgress(progress);
+        }
     }
 }
